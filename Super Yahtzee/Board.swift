608f9c07fd82cc666970d1b705750fcfd5162bbe
Anders Ravn <aravn4@gmail.com>
2018-09-29 09:46:44 +0200

    Added TextFields to PlayCells

--- Super Yahtzee/Board.swift
+++ Super Yahtzee/Board.swift
@@ -16,7 +16,7 @@ class Board {
     var calcFields: [String]!
     var specialPlays: [String]!
     var total: [String]!
-    var playCells: [[String]]! = []
+    var playCells: [[UITextField]]! = [] // Totals to the number of players.
     
     init(playerNames: [String]) {
         self.playerNames = playerNames
@@ -26,15 +26,32 @@ class Board {
         self.specialPlays = ["1 pair", "2 pairs", "3 pairs", "4 pairs", "2 x 3 equal", "1-2-3-4-5", "2-3-4-5-6", "1-2-3-4-5-6", "Full house", "Chance", "Super Yatzy"]
         self.total = ["Total"]
         for _ in self.playerNames {
-            var playerCells: [String]! = []
-            for _ in 0...self.getInfoColumn().count {
-                playerCells.append("4")
+            var playerCells: [UITextField]! = [] // Totals to the number of vertical cells.
+            for (index, _) in self.getInfoColumn().enumerated() {
+                if index == 6 || index == 7 || index == 19 {
+                    playerCells.append(createTextField(isUserInteractionEnabled: false))
+                    continue
+                }
+                playerCells.append(createTextField(isUserInteractionEnabled: true))
             }
             playCells.append(playerCells)
         }
     }
     
     func getInfoColumn() -> [String] {
-        return self.sumPlays + self.calcFields + self.specialPlays + self.total
+        return [""] + self.sumPlays + self.calcFields + self.specialPlays + self.total
+    }
+    
+    func createTextField(isUserInteractionEnabled: Bool) -> UITextField {
+        let textField = UITextField(frame: CGRect(x: 0, y: 0, width: 40.00, height: 20.00))
+        // textField.backgroundColor = UIColor(red:0.57, green:0.80, blue:0.64, alpha:1.0)
+        // textField.textColor = UIColor.white
+        // textField.placeholder = "Player \(playerNumber + 1)"
+        // textField.borderStyle = UITextBorderStyle.roundedRect
+        textField.font = UIFont(name: (textField.font?.fontName)!, size: 10.0)
+        textField.textAlignment = .center
+        textField.keyboardType = UIKeyboardType.numberPad
+        textField.isUserInteractionEnabled = isUserInteractionEnabled
+        return textField
     }
 }
--- Super Yahtzee/GameViewController.swift
+++ Super Yahtzee/GameViewController.swift
@@ -10,28 +10,6 @@ import Foundation
 import UIKit
 import SpreadsheetView
 
-struct Points {
-    var ones = 0
-    var twos = 0
-    var threes = 0
-    var fours = 0
-    var fives = 0
-    var sixes = 0
-    var sum = 0
-    var bonus = 0
-    var pairs1 = 0
-    var pairs2 = 0
-    var pairs3 = 0
-    var pairs4 = 0
-    var twoXThreeEqual = 0
-    var low = 0
-    var high = 0
-    var cameron = 0
-    var fullHouse = 0
-    var chance = 0
-    var total = 0
-}
-
 class GameViewController: UIViewController, SpreadsheetViewDataSource, SpreadsheetViewDelegate {
     
     @IBOutlet weak var spreadsheetView: SpreadsheetView!
@@ -66,7 +44,7 @@ class GameViewController: UIViewController, SpreadsheetViewDataSource, Spreadshe
     }
     
     func spreadsheetView(_ spreadsheetView: SpreadsheetView, widthForColumn column: Int) -> CGFloat {
-        if case 0 = column {
+        if case 0 = column { // The first column should be wider to fit play names.
             return 80
         } else {
             return 40
@@ -90,16 +68,17 @@ class GameViewController: UIViewController, SpreadsheetViewDataSource, Spreadshe
         // First column
         if indexPath.column == 0 {
             let cell = spreadsheetView.dequeueReusableCell(withReuseIdentifier: String(describing: InfoCell.self), for: indexPath) as! InfoCell
-            cell.label.text = self.board.getInfoColumn()[indexPath.row - 1]
+            cell.label.text = self.board.getInfoColumn()[indexPath.row]
             if (indexPath.row % 2 != 0) {
                 cell.label.backgroundColor = UIColor(red:0.57, green:0.80, blue:0.64, alpha:0.4)
             }
+            print(indexPath.row)
             return cell
         }
         
         if (indexPath.column > 1 && indexPath.row > 0) {
             let cell = spreadsheetView.dequeueReusableCell(withReuseIdentifier: String(describing: PlayCell.self), for: indexPath) as! PlayCell
-            cell.label.text = board.playCells[indexPath.column - 2][indexPath.row - 1]
+            cell.addSubview(board.playCells[indexPath.column - 2][indexPath.row - 1])
             if (indexPath.row % 2 != 0) {
                 cell.label.backgroundColor = UIColor(red:0.57, green:0.80, blue:0.64, alpha:0.4)
             }
